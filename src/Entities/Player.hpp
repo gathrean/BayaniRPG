0e21e11bda3a2bf4c1ea6f04da12a5012c3fb52d
Gathrean Dela Cruz <gathrean@icloud.com>
2023-12-23 15:52:45 -0800

    Organized Player header file

---
+++ src/Entities/Player.hpp
@@ -0,0 +1,38 @@
+//
+// Created by Gathrean Dela Cruz on 2023-12-23.
+//
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Projectile.hpp"
+
+class Player {
+
+private:
+
+    float playerRadius = 20.f;
+
+    sf::CircleShape playerShape;
+
+    std::vector<Projectile> playerProjectiles;
+
+public:
+
+    // Constructor and Destructor
+    Player();
+    ~Player() = default;
+
+    // Drawing methods
+    void drawPlayer(sf::RenderWindow &window);
+    void drawProjectiles(sf::RenderWindow &window);
+
+    // Player Position-related methods
+    void move(float offsetX, float offsetY);
+    void setPlayerPosition(sf::Vector2f position);
+    sf::Vector2f getPlayerPosition() const;
+
+    // Projectile-related methods
+    void shootProjectiles(sf::Vector2f target);
+    void updateProjectiles(unsigned int windowWidth, unsigned int windowHeight);
+};
