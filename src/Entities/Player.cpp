ead4cf02690b504cb05f666715aca8a53f13c19e
Gathrean Dela Cruz <gathrean@icloud.com>
2023-12-23 16:01:07 -0800

    Cleaned up the code even more for easier readability (and Added necessary comments)

--- src/Entities/Player.cpp
+++ src/Entities/Player.cpp
@@ -5,55 +5,68 @@
 #include "Player.hpp"
 
 Player::Player() {
-    shape.setRadius(playerRadius);
-    shape.setFillColor(sf::Color::Green);
+    playerShape.setRadius(playerRadius);
+    playerShape.setFillColor(sf::Color::Cyan);
 }
 
-void Player::draw(sf::RenderWindow &window) {
-    window.draw(shape);
+void Player::drawPlayer(sf::RenderWindow &window) {
+    window.draw(playerShape);
 }
 
 void Player::move(float x, float y) {
-    shape.move(x, y);
+    playerShape.move(x, y);
 }
 
 void Player::setPlayerPosition(sf::Vector2f position) {
-    shape.setPosition(position);
+    playerShape.setPosition(position);
 }
 
 sf::Vector2f Player::getPlayerPosition() const {
-    return shape.getPosition();
+    return playerShape.getPosition();
 }
 
 void Player::shootProjectiles(sf::Vector2f target) {
-    sf::Vector2f playerPos = shape.getPosition();
-    sf::Vector2f direction = target - playerPos;
+    sf::Vector2f playerPosition = playerShape.getPosition();
+    sf::Vector2f direction = target - playerPosition;
 
     float magnitude = sqrt(direction.x * direction.x + direction.y * direction.y);
+
+    // This is to prevent division by zero (normalize the direction vector)
     if (magnitude != 0) {
-        direction /= magnitude; // Normalize the direction vector
+        direction /= magnitude;
     }
 
-    // Calculate the position of the center of the player's shape
-    sf::Vector2f centerPos = playerPos + sf::Vector2f(shape.getRadius(), shape.getRadius());
-
-    // Adjust the projectile starting position relative to the center of the player's shape
     Projectile projectile;
-    projectile.setPosition(centerPos);
-    projectile.setVelocity(direction * 0.2f); // Set the velocity of the projectile
 
-    playerProjectiles.push_back(projectile); // Add the projectile to the vector
+    // This makes it so that the bullets 'spawn' from the center of the player's shape
+    sf::Vector2f centerPosition = playerPosition + sf::Vector2f(playerShape.getRadius(), playerShape.getRadius());
+
+    projectile.setPosition(centerPosition);
+    projectile.setVelocity(direction * 0.2f);
+
+    // After the player shoots a projectile, we keep track of it
+    playerProjectiles.push_back(projectile);
 }
 
 void Player::updateProjectiles(unsigned int windowWidth, unsigned int windowHeight) {
-    for (size_t i = 0; i < playerProjectiles.size(); ++i) {
-        playerProjectiles[i].update();
+    auto floatWindowWidth = static_cast<float>(windowWidth);
+    auto floatWindowHeight = static_cast<float>(windowHeight);
 
-        // Remove projectiles that are out of bounds
-        sf::Vector2f projPos = playerProjectiles[i].getPosition();
-        if (projPos.x < 0 || projPos.x > windowWidth || projPos.y < 0 || projPos.y > windowHeight) {
-            playerProjectiles.erase(playerProjectiles.begin() + i);
-            --i;
+    auto it = playerProjectiles.begin();
+
+    // While the iterator hasn't reached the end of the vector
+    while (it != playerProjectiles.end()) {
+        it->update();
+
+        sf::Vector2f projPos = it->getPosition();
+
+        bool outOfBoundsX = (projPos.x < 0) || (projPos.x > floatWindowWidth);
+        bool outOfBoundsY = (projPos.y < 0) || (projPos.y > floatWindowHeight);
+
+        if (outOfBoundsX || outOfBoundsY) {
+            it = playerProjectiles.erase(it);
+        } else {
+            ++it;
         }
     }
 }
