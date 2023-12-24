ce20795ee8eab48d1fec060f95c2069a7f75d0db
Gathrean Dela Cruz <gathrean@icloud.com>
2023-12-23 19:06:23 -0800

    Made drawWindow a bit more readable

--- src/Window/Window.cpp
+++ src/Window/Window.cpp
@@ -11,13 +11,19 @@ void Window::drawWindow(Player player) {
     backgroundX -= player.getDirection().x;
     backgroundY -= player.getDirection().y;
 
+    unsigned int textureWidth = backgroundTexture.getSize().x;
+    unsigned int textureHeight = backgroundTexture.getSize().y;
+
     // Tile the background image
-    int offsetX = static_cast<int>(std::fmod(backgroundX, backgroundTexture.getSize().x));
-    int offsetY = static_cast<int>(std::fmod(backgroundY, backgroundTexture.getSize().y));
+    int offsetX = static_cast<int>(std::fmod(backgroundX, textureWidth));
+    int offsetY = static_cast<int>(std::fmod(backgroundY, textureHeight));
+
+    unsigned int windowWidth = window.getSize().x;
+    unsigned int windowHeight = window.getSize().y;
 
     // Draw the tiled background image in both directions for a seamless effect
-    for (int x = offsetX; x < window.getSize().x; x += backgroundTexture.getSize().x) {
-        for (int y = offsetY; y < window.getSize().y; y += backgroundTexture.getSize().y) {
+    for (unsigned int x = offsetX; x < windowWidth; x += textureWidth) {
+        for (unsigned int y = offsetY; y < windowHeight; y += textureHeight) {
             backgroundSprite.setPosition(x, y);
             window.draw(backgroundSprite);
         }
